78b7df757907777e14d85f8b136dc026bc290c67
mrobinson <mrobinson@plaid.com>
2026-01-06 16:39:30 -0800

    Upgrade VPC module

--- modules/lambda/fargate.tf
+++ modules/lambda/fargate.tf
@@ -12,7 +12,7 @@ data "aws_region" "current" {}
 module "vpc" {
   count   = var.use_existing_subnets ? 0 : 1
   source  = "terraform-aws-modules/vpc/aws"
-  version = ">= 5.21.0"
+  version = ">= 5.21.0, < 6.0.0"
 
   name = "autospotting-${module.label.id}"
 
